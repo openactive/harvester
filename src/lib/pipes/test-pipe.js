07a48f96c21a1d2f0a98ada7030efd7a725d2da6
Michael Wood <michael.wood@opendataservices.coop>
2020-02-26 20:30:54 +0000

    test-pipe: Add a cached counter for testing purposes

--- src/lib/pipes/test-pipe.js
+++ src/lib/pipes/test-pipe.js
@@ -1,10 +1,13 @@
 import Pipe from './pipe.js';
+import { cache } from '../utils.js';
 
 class TestPipe extends Pipe {
 
   run(){
     return new Promise(async resolve => {
       console.log(`Running ${this.augmentedActivity.id} - ${this.augmentedActivity.data.name} through ${this.constructor.name}`);
+      cache.counter++;
+      console.log(cache);
       this.augmentedActivity['test-data'] = '1234';
       resolve(this.augmentedActivity);
     });
