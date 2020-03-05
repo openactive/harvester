247915b166182743718027e16a3e20debcff30a0
James Baster <james.baster@opendataservices.coop>
2020-03-05 16:01:52 +0000

    settings: More env variables

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -3,10 +3,10 @@ const Settings = {
   "elasticsearchUsername": process.env.ELASTICSEARCH_USERNAME,
   "elasticsearchPassword": process.env.ELASTICSEARCH_PASSWORD,
   "elasticsearchURL": process.env.ELASTICSEARCH_URL || 'http://localhost:9200',
-  "elasticIndexRaw": "open-active-raw",
-  "elasticIndexStage1State": "open-active-raw-harvester-state",
-  "elasticIndexNormalised": "open-active-normalised",
-  "elasticIndexStage2State": "open-active-stage-2-state",
+  "elasticIndexRaw": process.env.ELASTICSEARCH_INDEX_RAW || "open-active-raw",
+  "elasticIndexStage1State": process.env.ELASTICSEARCH_INDEX_STAGE_1_STATE || "open-active-raw-harvester-state",
+  "elasticIndexNormalised": process.env.ELASTICSEARCH_INDEX_NORMALISED || "open-active-normalised",
+  "elasticIndexStage2State": process.env.ELASTICSEARCH_INDEX_STAGE_2_STATE || "open-active-stage-2-state",
 
   "registryURL": "https://raw.githubusercontent.com/odscjames/openactive-sources/master/datasets.json",
   // For dev, can also do
