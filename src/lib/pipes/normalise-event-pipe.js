95b99683c885970aca988ed9906a6e8161afd3e2
rhiaro <amy@rhiaro.co.uk>
2020-03-07 13:25:37 +0100

    normalise: parse Events from subEvent of higher level types

--- src/lib/pipes/normalise-event-pipe.js
+++ src/lib/pipes/normalise-event-pipe.js
@@ -14,31 +14,97 @@ class NormaliseEventPipe extends Pipe {
       let data = this.rawData.data
       if (data.type == 'Event'){
 
-        let activities = this.parse_activity(data.activity);
-        let location = this.parse_location(data.location);
-
-        let normalisedEvent = new NormalisedEvent({
-          "data_id": data.id,
-          "name": data.name,
-          "description": data.description,
-          "event_status": data.eventStatus,
-          "location": location,
-          "activity": activities,
-          "start_date": data.startDate,
-          "end_date": data.endDate,
-          "organizer": data.organizer.name,
-          "derived_from_type": data.type,
-          "derived_from_id": this.rawData.id,
-        }, data);
-
+        let normalisedEvent = this.parseEvent(data);
         this.normalisedEvents.push(normalisedEvent);
+        // TODO: in theory regular Events might have subEvent or superEvent
+
+      }else if (data.subEvent !== undefined){
+
+        let subEvents = [];
+        if (!Array.isArray(data.subEvent)){
+          subEvents = [data.subEvent];
+        }else{
+          subEvents = data.subEvent;
+        }
+
+        const pipe = this;
+        subEvents.forEach(function(subEventData){
+          if(subEventData.type == 'Event'){
+
+            // Get data from parent
+            let parentEvent = pipe.parseEvent(data);
+            let normalisedEvent = pipe.parseEvent(subEventData, parentEvent);
+
+            pipe.normalisedEvents.push(normalisedEvent);
+
+            // If parent is an Event, HeadlineEvent or CourseInstance, also create
+            // a separate Event for it
+            let parentType = parentEvent.body.derived_from_type;
+            if (pipe.is_bookable(parentType)){
+              pipe.normalisedEvents.push(parentEvent);
+            }
+          }
+        });
 
       }else{
-        this.log(`Not processing ${this.rawData.data['type']} data here`);
+        this.log(`Pass: ${this.rawData.data['type']}`);
       }
 
       resolve(this.normalisedEvents);
     });
+  }
+
+  parseEvent(eventData, parentEvent){
+    try{
+      let activities = this.parse_activity(eventData.activity);
+      let location = this.parse_location(eventData.location);
+      // TODO fixme
+      let organizer = eventData.organizer;
+      if (organizer === undefined){
+        organizer = '';
+      }else{
+        organizer = organizer.name;
+      }
+
+      let normalisedEvent = new NormalisedEvent({
+        "data_id": eventData.id,
+        "name": eventData.name,
+        "description": eventData.description,
+        "event_status": eventData.eventStatus,
+        "location": location,
+        "activity": activities,
+        "start_date": eventData.startDate,
+        "end_date": eventData.endDate,
+        "organizer": organizer,
+        "derived_from_type": eventData.type,
+        "derived_from_id": this.rawData.id,
+      }, eventData);
+
+      if (parentEvent !== undefined){
+        // Copy these properties from the parent event to the NormalisedEvent made from the
+        // ScheduledSession only if they don't already exist there.
+        let propertiesToCopy = this.propertiesToCopy();
+        const pipe = this;
+        propertiesToCopy.forEach(function(property){
+          normalisedEvent = pipe.copyPropertyFromSuper(normalisedEvent, parentEvent, property);
+        });
+
+        normalisedEvent.body.derived_from_parent_type = parentEvent.body.derived_from_type;
+        normalisedEvent.body.derived_from_parent_id = parentEvent.body.derived_from_id;
+      }
+
+      return normalisedEvent;
+    }catch(e){
+      console.log(`Error parsing event ${this.rawData.id} :${e}`);
+    }
+  }
+
+  /**
+  Properties that should be copied from a HeadlineEvent or CourseInstance to an Event
+  (if they don't already exist).
+  **/
+  propertiesToCopy(){
+    return ['name', 'event_status', 'description', 'location', 'organizer'];
   }
 
 }
