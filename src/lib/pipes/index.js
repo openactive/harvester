eb0a1e435e452981fdab5d9d159b1359dc7fff19
rhiaro <amy@rhiaro.co.uk>
2020-03-09 23:45:26 +0100

    normalise: Schedule pipe

--- src/lib/pipes/index.js
+++ src/lib/pipes/index.js
@@ -3,12 +3,14 @@ import GeoPipe from './geo-pipe.js';
 import NormaliseEventPipe from './normalise-event-pipe.js';
 import NormaliseSlotPipe from './normalise-slot-pipe.js';
 import NormaliseScheduledSessionPipe from './normalise-scheduledsession-pipe.js';
+import NormaliseSchedulePipe from './normalise-schedule-pipe.js';
 
 export default [
   // Comment this out to see some test normalised events in the system
   // TestPipe
-  NormaliseEventPipe,
-  NormaliseSlotPipe,
-  NormaliseScheduledSessionPipe,
-  GeoPipe
+  // NormaliseEventPipe,
+  // NormaliseSlotPipe,
+  // NormaliseScheduledSessionPipe,
+  NormaliseSchedulePipe,
+  // GeoPipe
 ];
--- src/lib/pipes/normalise-schedule-pipe.js
+++ src/lib/pipes/normalise-schedule-pipe.js
@@ -2,6 +2,7 @@ import Pipe from './pipe.js';
 import { cache } from '../utils.js';
 import NormalisedEvent from '../normalised-event.js';
 import fetch from 'node-fetch';
+import { RRule, RRuleSet, rrulestr } from 'rrule';
 
 /** 
 The NormaliseSchedule Pipe turns OpenActive Schedule objects into a
@@ -14,6 +15,11 @@ Use the datetimes to generate individual event ids according to the idTemplate
 Populate name, description, eventStatus from the parent Schedule
 Add derived_from_type = Schedule and derived_from_id (original Schedule id)
 
+Publisher notes:
+* The value of eventSchedule should always be an array, but often isn't.
+* The modelling spec uses `repeatFrequency` but some publishers use `frequency`
+* urlTemplate, idTemplate and scheduledEventType should be present but aren't
+
 Some schedules don't have an end date, just +1 month
 
 **/
@@ -21,36 +27,169 @@ class NormaliseSchedulePipe extends Pipe {
   run(){
     return new Promise(async resolve => {
 
-      let data = this.rawData.data
-      if (data.type == 'Event'){
+      let pipe = this;
+      let data = this.rawData.data;
+      console.log(this.rawData.id);
+      // Find all the places Schedules can hide.
+      if (data.type == 'Schedule'){
+        console.log('top level.....');
+        console.log(data);
 
-        let activities = this.parse_activity(data.activity);
-        let location = this.parse_location(data.location);
+      }else if (data.eventSchedule !== undefined){
+        // anything.eventSchedule covers:
+        // Event, HeadlineEvent, CourseInstance, EventSeries, SessionSeries.
+        // SessionSeries must have eventSchedule OR subEvent, so we do not
+        //  need to check subEvents for schedules
+        console.log(`eventSchedule.....`);
+        console.log(data.eventSchedule);
+        // make parent NormalisedEvent
+          // (if bookable, push)
+          // foreach eventSchedule
+            // pipe.parseSchedule(scheduleData, parent);
 
-        let normalisedEvent = new NormalisedEvent({
-          "origin_id": data.id,
-          "name": data.name,
-          "description": data.description,
-          "event_status": data.eventStatus,
-          "location": location,
-          "activity": activities,
-          "start_date": data.startDate,
-          "end_date": data.endDate,
-          "organizer": data.organizer.name,
-          "derived_from_type": data.type,
-          "derived_from_id": this.rawData.id,
-        }, data);
-
-        this.normalisedEvents.push(normalisedEvent);
+      }else if (data.subEvent !== undefined){
+        // Check in nested subEvents
+        data.subEvent.forEach(function(event){
+          if (event.eventSchedule !== undefined){
+            console.log(`subEvent eventSchedule.....`);
+            console.log(event.eventSchedule);
+            // make parent NormalisedEvent
+            // (if bookable, push)
+            // what about parent parent?
+            // foreach eventSchedule
+              // pipe.parseSchedule(scheduleData, parent);
+          }
+        });
 
       }else{
-        this.log(`Not processing ${this.rawData.data['type']} data here`);
+        // nb. Slots and FacilityUse do not use eventSchedule.
+        this.log(`Pass: ${this.rawData.data['type']}`);
       }
 
       resolve(this.normalisedEvents);
     });
   }
 
+  parseSchedule(scheduleData, parentEvent){
+    let normalisedEventData = {};
+    //   "data_id": scheduleData.id,
+    //   "start_date": scheduleData.startDate,
+    //   "end_date": scheduleData.endDate,
+    
+    // Get data from parent
+    // name, description, eventStatus, organizer, location, activity
+    //   "derived_from_type": scheduleData.type (Schedule),
+    //   "derived_from_id": this.rawData.id,
+    //   "derived_from_parent_type": parentEvent.derived_from_type,
+    //   "derived_from_parent_id": parentEvent.derived_from_id
+    //   "part_of_id": parentEvent.id() only if isBookable
+
+    let allDates = this.generateDatesFromSchedule(scheduleData);
+    allDates.forEach(function(dates){
+      // Update start_date and end_date from dates
+      // normalisedEventData['start_date'] =
+      // normalisedEventData['end_date'] =
+      let normalisedEvent = new NormalisedEvent(normalisedEventData, this.rawData.data);
+      this.normalisedEvents.push(normalisedEvent);
+    });
+
+    // Should be able to get useful info from the following if anyone used them..
+    // urlTemplate
+    // idTemplate
+    // scheduledEventType
+
+  }
+
+  generateDatesFromSchedule(scheduleData){
+    let startDate = new Date(scheduleData.startDate);
+    let endDate = new Date(scheduleData.endDate);
+    let startTime = new Date(scheduleData.startTime);
+    let endTime = new Date(scheduleData.endTime);
+
+    // Make datetimes from above by gluing dates and times together
+
+    if (endDate === undefined && count === undefined){
+      // endDate = start date + 1 month
+    }
+    let rule = new RRule({
+      dtstart: startDate,
+      until: endDate
+    });
+
+    if (scheduleData.repeatFrequency !== undefined){
+      let freq = scheduleData.repeatFrequency;
+    }else if (scheduleData.frequency !== undefined){
+      let freq = scheduleData.frequency;
+    }
+    if(freq !== undefined){
+      let frequency = parseFrequency(freq);
+      rule['freq'] = frequency;
+    }
+
+    if (scheduleData.interval !== undefined){
+      rule['interval'] = scheduleData.interval;
+    }
+
+    if (scheduleData.byDay !== undefined){
+      let byDay = parseByWeekday(scheduleData.byDay);
+      rule['byweekday'] = byDay;
+    }
+
+    if (scheduleData.byMonth !== undefined){
+      rule['bymonth'] = scheduleData.byMonth;
+    }
+
+    if (scheduleData.byMonthDay !== undefined){
+      rule['bymonthday'] = scheduleData.byMonthDay;
+    }
+
+    if (scheduleData.repeatCount !== undefined){
+      rule['count'] = scheduleData.repeatCount;
+    }
+     
+    let allDates = [];
+    // Get all occurrence dates (Date instances):
+    // allDates = rule.all();
+
+    // Remove dates that are explicitly excluded.
+    if (scheduleData.exceptDate !== undefined){
+      scheduleData.exceptDate.forEach(function(exclude){
+        if (allDates.includes(exclude)){
+          allDates.splice (allDates.indexOf(exclude), 1);
+        }
+      });
+    }
+    
+    return allDates;
+
+  }
+
+  /**
+  Take the input from repeatFrequency property of Schedule and
+  return one of:
+
+    RRule.YEARLY
+    RRule.MONTHLY
+    RRule.WEEKLY
+    RRule.DAILY
+    RRule.HOURLY
+    RRule.MINUTELY
+    RRule.SECONDLY
+
+  **/
+  parseFrequency(frequency){
+    return;
+
+  }
+
+  /**
+  Take input (array) from byDay property of Scheduule and return one of
+  the weekday constants (RRule.MO, RRule.TU, etc)
+  **/
+  parseByWeekday(days){
+    return;
+  }
+
 }
 
-export default NormaliseEventPipe;
+export default NormaliseSchedulePipe;
