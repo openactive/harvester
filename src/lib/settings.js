5d35ba0e57ecc2ce5b82e4984e4f9d65bcac72a5
James Baster <james.baster@opendataservices.coop>
2020-03-10 16:24:57 +0000

    elastic: make index names consistent

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -3,10 +3,10 @@ const Settings = {
   "elasticsearchUsername": process.env.ELASTICSEARCH_USERNAME,
   "elasticsearchPassword": process.env.ELASTICSEARCH_PASSWORD,
   "elasticsearchURL": process.env.ELASTICSEARCH_URL || 'http://localhost:9200',
-  "elasticIndexRaw": process.env.ELASTICSEARCH_INDEX_RAW || "open-active-raw",
-  "elasticIndexStage1State": process.env.ELASTICSEARCH_INDEX_STAGE_1_STATE || "open-active-raw-harvester-state",
-  "elasticIndexNormalised": process.env.ELASTICSEARCH_INDEX_NORMALISED || "open-active-normalised",
-  "elasticIndexStage2State": process.env.ELASTICSEARCH_INDEX_STAGE_2_STATE || "open-active-stage-2-state",
+  "elasticIndexRaw": process.env.ELASTICSEARCH_INDEX_RAW || "harvester-raw",
+  "elasticIndexStage1State": process.env.ELASTICSEARCH_INDEX_STAGE_1_STATE || "harvester-stage-1-state",
+  "elasticIndexNormalised": process.env.ELASTICSEARCH_INDEX_NORMALISED || "harvester-normalised",
+  "elasticIndexStage2State": process.env.ELASTICSEARCH_INDEX_STAGE_2_STATE || "harvester-stage-2-state",
 
   "registryURL": "https://raw.githubusercontent.com/openactive/harvester/master/datasets.json",
   // For dev, can also do
