944ce0741cdd941c8e8830aa527fad52c9d91d9d
James <james.baster@opendataservices.coop>
2020-05-06 11:38:24 +0100

    express: Add Asset files

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
+// Import Libs
 import express from 'express'
+import path from 'path';
+
+// Import our code
 import Settings from './src/lib/settings.js';
 
+// Setup
+const __dirname = path.resolve();
 const app = express();
 
+// Setup Dynamic pages
+// TODO
+
+// Setup Assets
+app.use(express.static(path.join(__dirname, 'web-assets')));
+
+// Run server
 app.listen(Settings.webServerPort, () => { console.log("started web server on port " + Settings.webServerPort); } );
