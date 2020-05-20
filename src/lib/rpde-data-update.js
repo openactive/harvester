090b3735c7960845a2240c02fc4460da014ed71b
Timo Hill <tim.hill@theodi.org>
2020-05-20 22:24:55 +0100

    Coping with JSON-LD @ prefixing on stage 1 ingest

--- src/lib/rpde-data-update.js
+++ src/lib/rpde-data-update.js
@@ -5,8 +5,8 @@ class RPDEItemUpdate extends RPDEItem {
     super(activityItem, publisherKey, feedKey)
     this.data = activityItem.data;  // capturing entire data object
     this.kind = activityItem.kind;
-    this.data_id = ('id' in activityItem.data ? activityItem.data.id : '');
-    this.data_type = ('type' in activityItem.data ? activityItem.data.type : '');
+    this.data_id = ('id' in activityItem.data ? activityItem.data.id : activityItem.data['@id']);
+    this.data_type = ('type' in activityItem.data ? activityItem.data.type : activityItem.data['@type']);
     this.deleted = false;
   }
 }
